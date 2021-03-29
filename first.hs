077332dc00924562d19b857a073274239031ac2e
tankorsmash <tankorsmash@gmail.com>
2021-03-29 13:52:14 -0400

    write my own print because lazy

--- first.hs
+++ first.hs
@@ -13,4 +13,7 @@ get_first_letter str = str !! 0
 -- main = putStrLn $ show $ length "Josh"
 -- main = putStrLn $ show $ null [1, 2, 3]
 -- main = putStrLn $ show $ reverse "Josh"
-main = putStrLn $ show [2, 4..20] --make sure you give it a valid first set of two numbers, otherwise it goes forever with `[2, 1..2]`.
+-- main = putStrLn $ show [2, 4..20] --make sure you give it a valid first set of two numbers, otherwise it goes forever with `[2, 1..2]`.
+
+mprint x = putStrLn $ show x
+main = mprint "ASDASD"
