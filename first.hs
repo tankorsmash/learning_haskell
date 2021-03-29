9ef30bf0883367de86633eda89df02666aa8c667
tankorsmash <tankorsmash@gmail.com>
2021-03-29 13:18:17 -0400

    learning about lists

--- first.hs
+++ first.hs
@@ -1,4 +1,12 @@
 doubleMe x = x + x
 my_int = doubleMe 2
 doubleSmallNumber x = if x > 100 then x else x * 2
-main = putStrLn $ show $ doubleSmallNumber 50
+
+lostNumbers = [4,8,15,16,23,42]
+
+add_list = [1] ++ [2]
+
+get_first_letter str = str !! 0
+
+-- main = putStrLn $ show $ doubleSmallNumber 50
+main = putStrLn $ show $ get_first_letter "Josh"
