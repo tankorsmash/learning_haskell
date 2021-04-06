79c3db5ccd3926af8d3d627662971d6e95e45859
tankorsmash <tankorsmash@gmail.com>
2021-04-06 15:40:24 -0400

    copypasting types and dataclasses more
    im real lost now

--- first.hs
+++ first.hs
@@ -180,5 +180,50 @@ print_circle (circle) = "ASDADS"
 -- show :: Circle -> String
 -- show (Circle ox oy r) = "The origin's xy are: "
 
-my_circle = Circle 123 123 30
-main = print' $ print_circle my_circle
+-- my_circle = Circle 123 123 30
+-- main = print' $ print_circle my_circle
+
+
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+    case Map.lookup lockerNumber map of
+        Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist!"
+        Just (state, code) -> if state /= Taken
+                            then Right code
+                            else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+
+lockers :: LockerMap
+lockers = Map.fromList
+    [(100,(Taken,"ZD39I"))
+    ,(101,(Free,"JAH3I"))
+    ,(103,(Free,"IQSA9"))
+    ,(105,(Free,"QOTSA"))
+    ,(109,(Taken,"893JJ"))
+    ,(110,(Taken,"99292"))
+    ]
+
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+      | x < a  = Node a (treeInsert x left) right
+        | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+      | x < a  = treeElem x left
+        | x > a  = treeElem x right
